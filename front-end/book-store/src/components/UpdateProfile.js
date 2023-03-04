08b8472f5e88c94555393235edd8076976241f94
Khaoula ElHattabi <khaoulaelhattabi@gmail.com>
2023-03-04 22:20:23 +0100

    Update Profile v2

--- front-end/book-store/src/components/UpdateProfile.js
+++ front-end/book-store/src/components/UpdateProfile.js
@@ -4,6 +4,7 @@ import { Button } from 'primereact/button';
 import Navbar from './Navbar';
 import FileBase64 from 'react-file-base64';
 import userServices from "../services/userService";
+import { useNavigate ,Link} from "react-router-dom";
 
 
           
@@ -13,8 +14,8 @@ const UpdateProfile = (props) =>{
     const [lName, setLName] = useState("");
     const [fName, setFName] = useState("");
     const [email, setEmail] = useState("");
-    const [password, setPasswordr] = useState("");
-    
+    const [password, setPassword] = useState();
+    const [password1, setPassword1] = useState();
     const [image, setImage] = useState("");
 
     const [role, setRole] = useState("");
@@ -24,6 +25,7 @@ const UpdateProfile = (props) =>{
       setBase64String(file.base64);
     };
 
+    const n = useNavigate()
 
     //const usr = window.localStorage.getItem("user");
     const parsedData = JSON.parse(window.localStorage.getItem("user"));
@@ -38,9 +40,47 @@ const UpdateProfile = (props) =>{
       setFName(res.data.fName)
       setEmail(res.data.email)
       setRole(res.data.role)
-      console.log(email)
+      setBase64String(res.data.image)
+      setPassword(res.data.password)
+      console.log(password)
       }
 
+      async function submitUser (event) {
+         event.preventDefault();
+         if(password==0){
+
+         
+         const u={
+          "_id":i,
+          "uName": uName,
+          "lName": lName,
+          "fName": fName,
+          "email": email,
+          "password": password1,
+          "image": base64String,
+          "role": role,
+        } 
+        await userServices.updateUser(u)
+      }else{
+        const u={
+          "_id":i,
+          "uName": uName,
+          "lName": lName,
+          "fName": fName,
+          "email": email,
+          "password": password1,
+          "image": base64String,
+          "role": role,
+        }
+         await userServices.updateUser(u)
+      }
+      if(role=="admin"){
+        n("/admin")
+      }else{n("/user")}
+      }
+
+
+        
       useEffect(()=>{
         getUser()
     },[])
@@ -53,17 +93,17 @@ const UpdateProfile = (props) =>{
       Update Profile
     </h2>
         
-<form className="row g-3 needs-validation" noValidate style={{maxHeight: "300px", maxWidth:"1000px", margin:"auto",marginTop:"20px", paddingTop:"20px", border:"none"}}>
+<form onSubmit={(event)=>submitUser(event)}  className="row g-3 needs-validation" noValidate style={{maxHeight: "300px", maxWidth:"1000px", margin:"auto",marginTop:"20px", paddingTop:"20px", border:"none"}}>
         <div className="col-md-4">
           <label htmlFor="validationCustom01" className="form-label">User Name</label>
-          <input type="text" className="form-control" id="validationCustom01"  required  value={uName} />
+          <input type="text" className="form-control" id="validationCustom01"  required  value={uName}  onChange={event => setUName(event.target.value)}/>
           <div className="valid-feedback">
             Looks good!
           </div>
         </div>
         <div className="col-md-4">
           <label htmlFor="validationCustom02" className="form-label">First Name</label>
-          <input type="text" className="form-control" id="validationCustom02" required  value={fName}/>
+          <input type="text" className="form-control" id="validationCustom02" required  value={fName}  onChange={event => setFName(event.target.value)}/>
           <div className="valid-feedback">
             Looks good!
           </div>
@@ -72,7 +112,7 @@ const UpdateProfile = (props) =>{
           <label htmlFor="validationCustomUsername" className="form-label">Last Name</label>
           <div className="input-group has-validation">
             
-            <input type="text" className="form-control" id="validationCustomUsername" value={lName} aria-describedby="inputGroupPrepend" />
+            <input type="text" className="form-control" id="validationCustomUsername" value={lName}  onChange={event => setLName(event.target.value)}    aria-describedby="inputGroupPrepend" />
             <div className="invalid-feedback">
               Please choose a username.
             </div>
@@ -80,14 +120,14 @@ const UpdateProfile = (props) =>{
         </div>
         <div  className="col-7">
           <label htmlFor="validationCustom03" className="form-label">Email</label>
-          <input  value={email} type="text" className="form-control" id="validationCustom03" />
+          <input  value={email} onChange={event => setEmail(event.target.value)}  type="text" className="form-control" id="validationCustom03" />
           <div className="invalid-feedback">
             Please provide a valid city.
           </div>
         </div>
         <div className="col-5">
           <label htmlFor="validationCustom03" className="form-label">Password</label>
-          <input type="password" className="form-control" id="validationCustom03" />
+          <input type="password" className="form-control" id="validationCustom03"onChange={event => setPassword1(event.target.value)} />
           <div className="invalid-feedback">
             Please provide a valid city.
           </div>
